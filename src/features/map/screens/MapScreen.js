d9f3ad8d755b73680b8ef49b8b9d546553c1a18a
lazy-App <a.raheeem0003@gmail.com>
2025-01-01 11:29:28 +0100

    implemented map and map markers on location

--- src/features/map/screens/MapScreen.js
+++ src/features/map/screens/MapScreen.js
@@ -1,71 +1,68 @@
-import { React, useContext, useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
-import { MapView, Marker } from "react-native-maps";
+import React, { useContext, useEffect, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import MapView, { MapMarker } from "react-native-maps";
 
 import styled from "styled-components";
 import Search from "../components/Search";
 import { LocationContext } from "../../../services/location/locationContext";
 import { ResturantsContext } from "../../../services/resturants/resturantsContext";
 
-const SearchContainer = styled.View`
+const SearchContainer = styled(View)`
   padding: ${({ theme }) => theme.space[0]};
   z-index: 999;
   width: 100%;
 `;
 
+const Map = styled(MapView)`
+  width: 100%;
+  height: 100%;
+`;
+
+const Container = styled(View)`
+  flex: 1;
+`;
+
 const MapScreen = () => {
   const { resturants } = useContext(ResturantsContext);
   const { location } = useContext(LocationContext);
-  const { lat, lng, viewport } = location;
-  console.log(viewport);
+  const { lat, lng, viewport } = location || {};
+  const [latDelta, setLatDelta] = useState(0);
 
-  const [latDelta, setlatDelta] = useState(0);
   useEffect(() => {
-    const northeastLat = viewport.northeast.lat;
-    const southwestLat = viewport.southwest.lat;
-    setlatDelta(northeastLat - southwestLat);
-    console.log("lat delta: ", latDelta);
+    if (viewport) {
+      const northeastLat = viewport.northeast.lat;
+      const southwestLat = viewport.southwest.lat;
+      setLatDelta(northeastLat - southwestLat);
+      console.log(latDelta);
+    }
   }, [location, viewport]);
 
   return (
-    <View style={styles.container}>
+    <Container>
       <SearchContainer>
         <Search />
       </SearchContainer>
-      <MapView
-        style={styles.map}
+      <Map
         region={{
           latitude: lat,
           longitude: lng,
           latitudeDelta: latDelta,
-          longitudeDelta: 0.2,
+          longitudeDelta: 0.02,
         }}
       >
         {resturants.map((resturant) => {
-          return (
-            <Marker
-              key={resturant.name}
-              title={resturant.name}
-              coordinate={{
-                latitude: resturant.geometry.location.lat,
-                longitude: resturant.geometry.location.lng,
-              }}
-            />
-          );
+          <MapMarker
+            key={resturant.name}
+            title={resturant.name}
+            coordinate={{
+              latitude: resturant.geometry.location.lat,
+              longitude: resturant.geometry.location.lng,
+            }}
+          />;
         })}
-      </MapView>
-    </View>
+      </Map>
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: "100%",
-    height: "100%",
-  },
-});
 
 export default MapScreen;
